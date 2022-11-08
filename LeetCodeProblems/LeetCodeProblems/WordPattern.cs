51de22ecbf4aae0c942fbd76086f787710064385
OlaAtaiwi <olaabilal1@gmail.com>
2022-11-08 12:55:14 +0000

    Solving issues in WordPattern Class

--- LeetCodeProblems/LeetCodeProblems/WordPattern.cs
+++ LeetCodeProblems/LeetCodeProblems/WordPattern.cs
@@ -12,22 +12,27 @@ namespace LeetCodeProblems
            var words = s.Split(' ');
             if (words.Length != pattern.Length)
                 return false;
-            Dictionary<char, string> Dict = new Dictionary<char, string>();
+            var patternDict = new Dictionary<char, string>();
+            var wordsDict = new Dictionary<string,char>();
             for (int i = 0; i < pattern.Length; i++)
             {
-                if (Dict.ContainsKey(pattern[i]))
+                if (!patternDict.ContainsKey(pattern[i]))
                 {
-                    if (Dict[pattern[i]] != words[i])
+                    if (!wordsDict.ContainsKey(words[i]))
+                    {
+                        patternDict.Add(pattern[i], words[i]);
+                        wordsDict.Add(words[i], pattern[i]);
+                    }
+                    else
                         return false;
                 }
                 else
                 {
-                    if (Dict.ContainsValue(words[i]))
+                    if (patternDict[pattern[i]] != words[i])
                         return false;
-                    else
-                        Dict.Add(pattern[i], words[i]);
                 }
             }
+
             return true;
         }
     }
